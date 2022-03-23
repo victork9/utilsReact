3b7e724714e0c8ac712abb4caaabe31781d42982
victor klippel de souza <victorklippel@hotmail.com>
2022-03-23 13:50:05 -0300

    Update masks

--- utils/masks/index.ts
+++ utils/masks/index.ts
@@ -1,5 +1,9 @@
-export function formatData(value: string) {
-  return value.replace(/\D/g, "").replace(/(\d{2})(\d{2})(\d{4})/, "$1/$2/$3");
+export function maskDate(value: string) {
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '$1/$2')
+    .replace(/(\d{2})(\d)/, '$1/$2')
+    .replace(/(\d{4})(\d)/, '$1');
 }
 export function formatTimer(value: string) {
   return value.replace(/\D/g, "").replace(/(\d{2})(\d{2})/, "$1:$2");
@@ -12,4 +16,23 @@ export function formatCpf(value: string) {
     .replace(/(\d{3})(\d)/, "$1.$2")
     .replace(/(\d{3})(\d{1,2})/, "$1-$2")
     .replace(/(-\d{2})\d+?$/, "$1");
+}
+
+export function maskCnpj(value: string): string {
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,4})/, '$1/$2')
+    .replace(/(\d{4})(\d{1,2})/, '$1-$2');
+}
+
+export function maskPhone(value: string): string {
+  const variablePhones = (valuePhone: string) => {
+    return valuePhone.replace(valuePhone.length > 11 ? /(\d{5})(\d)/ : /(\d{4})(\d)/, '$1-$2');
+  };
+
+  const newValue = value.replace(/\D/g, '').replace(/(\d{2})(\d)/, '$1 $2');
+
+  return variablePhones(newValue);
 }
